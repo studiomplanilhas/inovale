9a7087fd2a4380b8bb34eb14e7f80dfdaad1ac93
Mauricio Maniely <mauriciomproduc@alu.ufc.br>
2023-09-30 16:20:55 -0300

    Primeiro commit

---
+++ inovalejr.js
@@ -0,0 +1,29 @@
+// Redirecionar para o Link 1 ao clicar no Botão 1
+function acaoBotao1() {
+    window.location.href = "https://www.link1.com";
+}
+
+// Redirecionar para o Link 2 ao clicar no Botão 2
+function acaoBotao2() {
+    window.location.href = "https://www.link2.com";
+}
+
+// Redirecionar para o Link 3 ao clicar no Botão 3
+function acaoBotao3() {
+    window.location.href = "https://www.link3.com";
+}
+
+// Redirecionar para o Link 4 ao clicar no Botão 4
+function acaoBotao4() {
+    window.location.href = "https://www.link4.com";
+}
+
+// Redirecionar para o Link 5 ao clicar no Botão 5
+function acaoBotao5() {
+    window.location.href = "https://www.link5.com";
+}
+
+// Redirecionar para o Link 6 ao clicar no Botão 6
+function acaoBotao6() {
+    window.location.href = "https://www.link6.com";
+}
