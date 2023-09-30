fcd1126f89f58f570c3dbbe1d44be6f3a6704adb
Studio M <146476340+studiomplanilhas@users.noreply.github.com>
2023-09-30 18:44:49 -0300

    Update inovalejr.js

--- inovalejr.js
+++ inovalejr.js
@@ -1,29 +1,29 @@
 // Redirecionar para o Link 1 ao clicar no Botão 1
 function acaoBotao1() {
-    window.location.href = "https://www.link1.com";
+    window.location.href = "https://docs.google.com/spreadsheets/d/1rUyPhy5Tew-jjBdxDn-WRBkAwYrluRsYv9WFog67PZ0/edit#gid=1556452013";
 }
 
 // Redirecionar para o Link 2 ao clicar no Botão 2
 function acaoBotao2() {
-    window.location.href = "https://www.link2.com";
+    window.location.href = "https://www.canva.com/folder/FAFar1_ZD9Y";
 }
 
 // Redirecionar para o Link 3 ao clicar no Botão 3
 function acaoBotao3() {
-    window.location.href = "https://www.link3.com";
+    window.location.href = "https://www.canva.com/design/DAFawEarsOk/_Rrxiwvvs62sOLWWtb6kVg/edit";
 }
 
 // Redirecionar para o Link 4 ao clicar no Botão 4
 function acaoBotao4() {
-    window.location.href = "https://www.link4.com";
+    window.location.href = "https://app.pipefy.com/organizations/164775";
 }
 
 // Redirecionar para o Link 5 ao clicar no Botão 5
 function acaoBotao5() {
-    window.location.href = "https://www.link5.com";
+    window.location.href = "https://drive.google.com/drive/u/1/folders/15NmoQEDFeuUXnQUaIAK9dzMGG_RNkNFF";
 }
 
 // Redirecionar para o Link 6 ao clicar no Botão 6
 function acaoBotao6() {
-    window.location.href = "https://www.link6.com";
+    window.location.href = "https://drive.google.com/drive/u/1/folders/1nX11M6KOu8jxC27n6cFci7DmtTCbZn3R";
 }
