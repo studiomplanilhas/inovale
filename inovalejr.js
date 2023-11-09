3a3d5b86ef7b7bfe1ac538cb1d033bf46b95ed22
Studio M <146476340+inovalejr@users.noreply.github.com>
2023-11-09 20:59:56 -0300

    Update inovalejr.js

--- inovalejr.js
+++ inovalejr.js
@@ -1,29 +1,27 @@
-// Redirecionar para o Link 1 ao clicar no Botão 1
-function planilhaInovale() {
-    window.location.href = "https://docs.google.com/spreadsheets/d/1rUyPhy5Tew-jjBdxDn-WRBkAwYrluRsYv9WFog67PZ0/edit#gid=1556452013";
+function criarBotoes() {
+    const meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+    const container = document.getElementById('calendarContainer');
+
+    meses.forEach((mes) => {
+        const button = document.createElement('button');
+        button.textContent = mes;
+        button.onclick = function () {
+            redirecionar('https://link-' + mes.toLowerCase());
+        };
+        button.style.fontSize = '18px';
+        button.style.backgroundColor = '#e65e0e';
+        button.style.color = '#fff';
+        button.style.border = 'none';
+        button.style.cursor = 'pointer';
+        button.style.width = '100%';
+        button.style.height = '50px';
+
+        container.appendChild(button);
+    });
 }
 
-// Redirecionar para o Link 2 ao clicar no Botão 2
-function canvaAdmFinanceiro() {
-    window.location.href = "https://www.canva.com/folder/FAFar1_ZD9Y";
+function redirecionar(link) {
+    window.location.href = link;
 }
 
-// Redirecionar para o Link 3 ao clicar no Botão 3
-function recibos() {
-    window.location.href = "https://www.canva.com/design/DAFawEarsOk/_Rrxiwvvs62sOLWWtb6kVg/edit";
-}
-
-// Redirecionar para o Link 4 ao clicar no Botão 4
-function pipefy() {
-    window.location.href = "https://app.pipefy.com/organizations/164775";
-}
-
-// Redirecionar para o Link 5 ao clicar no Botão 5
-function documentosDrive() {
-    window.location.href = "https://drive.google.com/drive/u/1/folders/15NmoQEDFeuUXnQUaIAK9dzMGG_RNkNFF";
-}
-
-// Redirecionar para o Link 6 ao clicar no Botão 6
-function documentosGerais() {
-    window.location.href = "https://drive.google.com/drive/u/1/folders/1nX11M6KOu8jxC27n6cFci7DmtTCbZn3R";
-}
+window.onload = criarBotoes;
